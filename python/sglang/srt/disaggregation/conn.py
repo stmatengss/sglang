220985b0deb1745904935ab40a5a6887c89c92c7
Teng Ma <stmatengss@163.com>
2025-03-25 20:54:30 +0800

    [Init] Add original proto

--- python/sglang/srt/disaggregation/conn.py
+++ python/sglang/srt/disaggregation/conn.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 import numpy as np
 import numpy.typing as npt
+# import mooncake_vllm_adaptor as mva
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +23,13 @@ class KVArgs:
 
 
 class KVManager:
-    def __init__(self, args: KVArgs): ...
+    def __init__(self, args: KVArgs):
+        # self.engine = mva.mooncake_vllm_adaptor()
+        # self.local_rank = local_rank
+
+        # self.config = MooncakeTransferEngineConfig.load_from_env()
+        # self.engine.initialize(local_hostname, metadata_server, protocol, device_name)
+        pass
 
 
 class KVPoll:
@@ -40,9 +47,15 @@ class KVSender:
     def init(self, num_kv_indices: int, aux_index: Optional[int] = None): ...
 
     def send(self, kv_indices: npt.NDArray[np.int32]):
+        # data = [kv_data_ptr[i] for i in kv_indices]
+        # buffer_src = allocateManagedBuffer(len)
+        # writeBytestoBuffer(buffer_src, data)
+        # transferSync(buffer_src, ???) # TODO Maybe it should be postBatchSend
+        # zmq.send(finished)
         self.has_sent = True
 
     def poll(self) -> KVPoll:
+        # pollCq?
         if self.has_sent is False:
             # Assume handshake completed instantly
             return KVPoll.WaitingForInput
@@ -61,6 +74,10 @@ class KVReceiver:
         self.has_init = False
 
     def init(self, kv_indices: npt.NDArray[np.int32], aux_index: Optional[int] = None):
+        # if (zmq.recv() is finished):
+        #     data = [kv_data_ptr[i] for i in kv_indices]
+        #     readBytesFromBuffer(buffer_dst, data)
+        #     self.has_init = True
         self.has_init = True
 
     def poll(self) -> KVPoll:
